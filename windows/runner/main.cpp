86dcb190231e8c6b8f3c9176a6703c54f34fb0c7
shinaaaa <shin029abc@gmail.com>
2022-08-19 09:03:34 +0900

    chore: 윈도우 애플리케이션 창크기 조절

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(800, 600);
+  Win32Window::Size size(800, 800);
   if (!window.CreateAndShow(L"slack_bundle", origin, size)) {
     return EXIT_FAILURE;
   }
