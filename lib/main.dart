13b3aa4e3f6b68d8b529112339ad5ea4ee6a3781
shinaaaa <shin029abc@gmail.com>
2022-08-28 17:31:17 +0900

    feat: title 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,9 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
       theme: ThemeData(
           fontFamily: "Nanum_Gothic",
           textTheme: const TextTheme(
