abc54926f5763c75341bfbc3a4cfd569c328b095
shinaaaa <shin029abc@gmail.com>
2022-08-29 20:42:44 +0900

    feat: Http 통신 공통 클래스로 변경

--- lib/service/SendMessageService.dart
+++ lib/service/SendMessageService.dart
@@ -1,18 +1,16 @@
-import 'dart:typed_data';
-
 import 'package:dio/dio.dart';
 
 import '../model/PostMessage.dart';
 import '../model/ScheduledMessage.dart';
+import '../util/HttpService.dart';
 import '../util/Preferences.dart';
 
 class SendMessageService {
   Future<bool> callPostMessage(String channel, String text) async {
     String token = await Preferences.getToken();
 
-    final response = await Dio().post('https://slack.com/api/chat.postMessage',
-        data: {"channel": channel, "text": text},
-        options: Options(headers: {"authorization": "Bearer $token"}));
+    final response = await HttpService.create().post('/chat.postMessage',
+        data: {"token": token, "channel": channel, "text": text});
 
     if (response.statusCode != 200) return false;
     PostMessage result = PostMessage.fromJson(response.data);
@@ -21,17 +19,16 @@ class SendMessageService {
 
   Future callFileUpload(
       String channel, String text, Stream<List<int>> fileStream) async {
+    String token = await Preferences.getToken();
     var formData = FormData.fromMap({
+      "token": token,
       "channels": channel,
       'file': fileStream,
       "initial_comment": text
     });
 
-    String token = await Preferences.getToken();
-
-    final response = await Dio().post('https://slack.com/api/files.upload',
-        data: formData,
-        options: Options(headers: {"authorization": "Bearer $token"}));
+    final response =
+        await HttpService.create().post('/files.upload', data: formData);
 
     if (response.statusCode != 200) return false;
     Slack result = Slack.fromJson(response.data);
@@ -41,10 +38,13 @@ class SendMessageService {
   Future<bool> callScheduleMessage(
       String channel, String time, String text) async {
     String token = await Preferences.getToken();
-    final response = await Dio().post(
-        'https://slack.com/api/chat.scheduleMessage',
-        data: {"channel": channel, "post_at": time, "text": text},
-        options: Options(headers: {"authorization": "Bearer $token"}));
+    final response = await HttpService.create().post('/chat.scheduleMessage',
+        data: {
+          "token": token,
+          "channel": channel,
+          "post_at": time,
+          "text": text
+        });
     if (response.statusCode != 200) return false;
     Slack result = Slack.fromJson(response.data);
     return result.ok;
@@ -53,10 +53,9 @@ class SendMessageService {
   Future<ScheduledMessage?> callScheduledMessagesList(String channel) async {
     String token = await Preferences.getToken();
 
-    final response = await Dio().post(
-        'https://slack.com/api/chat.scheduledMessages.list',
-        data: {"channel": channel},
-        options: Options(headers: {"authorization": "Bearer $token"}));
+    final response = await HttpService.create().post(
+        '/chat.scheduledMessages.list',
+        data: {"token": token, "channel": channel});
     if (response.statusCode != 200) return null;
     ScheduledMessage result = ScheduledMessage.fromJson(response.data);
     return result;
@@ -66,36 +65,17 @@ class SendMessageService {
       String channel, String messageId) async {
     String token = await Preferences.getToken();
 
-    final response = await Dio().post(
-        'https://slack.com/api/chat.deleteScheduledMessage',
-        data: {"channel": channel, "scheduled_message_id": messageId},
-        options: Options(headers: {"authorization": "Bearer $token"}));
+    final response = await HttpService.create()
+        .post('/chat.deleteScheduledMessage', data: {
+      "token": token,
+      "channel": channel,
+      "scheduled_message_id": messageId
+    });
 
     if (response.statusCode != 200) return false;
     Slack result = Slack.fromJson(response.data);
     return result.ok;
   }
-
-// void callFilesUpload() async {
-//   // file picker를 통해 파일 여러개 선택
-//   FilePickerResult? result =
-//   await FilePicker.platform.pickFiles(allowMultiple: true);
-//
-//   if (result != null) {
-//     final filePaths = result.paths;
-//
-//     // 파일 경로를 통해 formData 생성
-//     var dio = Dio();
-//     var formData = FormData.fromMap({
-//       'files': List.generate(filePaths,
-//               (index) => MultipartFile.fromFileSync(filePaths[index]!))
-//     });
-//
-//     // 업로드 요청
-//     final response = await dio.post('/upload', data: formData);
-//   } else {
-//     // 아무런 파일도 선택되지 않음.
-//   }
 }
 
 class Slack {
